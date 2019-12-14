cb2d5ad99fc4cc9d9107fd8caf9cbe2375de60a1
pambrose <pambrose@mac.com>
2019-12-14 11:37:17 -0800

    Misc cleanup

--- src/main/kotlin/org/athenian/chunker/ChunkerClient.kt
+++ src/main/kotlin/org/athenian/chunker/ChunkerClient.kt
@@ -17,176 +17,176 @@ import java.util.zip.CRC32
 
 
 class ChunkerClient internal constructor(private val channel: ManagedChannel) : Closeable {
-    private val blockingStub: ChunkerGrpc.ChunkerBlockingStub = ChunkerGrpc.newBlockingStub(channel)
-    private val asyncStub: ChunkerGrpc.ChunkerStub = ChunkerGrpc.newStub(channel)
-    private val semaphore = Semaphore(1)
+  private val blockingStub: ChunkerGrpc.ChunkerBlockingStub = ChunkerGrpc.newBlockingStub(channel)
+  private val asyncStub: ChunkerGrpc.ChunkerStub = ChunkerGrpc.newStub(channel)
+  private val semaphore = Semaphore(1)
 
-    constructor(host: String, port: Int = 50051) :
-            this(ManagedChannelBuilder.forAddress(host, port).usePlaintext().build())
+  constructor(host: String, port: Int = 50051) :
+      this(ManagedChannelBuilder.forAddress(host, port).usePlaintext().build())
 
-    fun uploadImage(filepath: String) {
-        require(filepath.contains(".")) { "File name missing type suffix: $filepath" }
+  fun uploadImage(filepath: String) {
+    require(filepath.contains(".")) { "File name missing type suffix: $filepath" }
 
-        val finishLatch = CountDownLatch(1)
-        var clientByteCount = 0
-        var clientChunkCount = 0
-        val clientChecksum = CRC32()
+    val finishLatch = CountDownLatch(1)
+    var clientByteCount = 0
+    var clientChunkCount = 0
+    val clientChecksum = CRC32()
 
-        val responseObserver =
-            object : StreamObserver<UploadImageResponse> {
-                override fun onNext(response: UploadImageResponse) {
-                    logger.debug { "Response:\n$response" }
+    val responseObserver =
+      object : StreamObserver<UploadImageResponse> {
+        override fun onNext(response: UploadImageResponse) {
+          logger.debug { "Response:\n$response" }
 
-                    // Check for checksum and length match
-                    logger.info { "Checksums ${response.checksum} and ${clientChecksum.value} are equal ${response.checksum == clientChecksum.value}" }
-                    logger.info { "Byte counts $clientByteCount and ${response.byteCount} are equal ${clientByteCount == response.byteCount}" }
+          // Check for checksum and length match
+          logger.info { "Checksums ${response.checksum} and ${clientChecksum.value} are equal ${response.checksum == clientChecksum.value}" }
+          logger.info { "Byte counts $clientByteCount and ${response.byteCount} are equal ${clientByteCount == response.byteCount}" }
 
-                    logger.info { "Releasing semaphore" }
-                    // Release semaphore to allow the next msg to be sent
-                    semaphore.release()
+          logger.info { "Releasing semaphore" }
+          // Release semaphore to allow the next msg to be sent
+          semaphore.release()
+        }
+
+        override fun onError(t: Throwable) {
+          val status = Status.fromThrowable(t)
+          logger.info { "uploadImage() failed: $status" }
+          finishLatch.countDown()
+        }
+
+        override fun onCompleted() {
+          finishLatch.countDown()
+        }
+      }
+
+    val requestObserver = asyncStub.uploadImage(responseObserver)
+
+    try {
+      val file = File(filepath)
+      if (!file.exists()) {
+        logger.info { "File does not exist" }
+        return
+      }
+
+      val filename = filepath.split("/").lastOrNull()!!
+
+      val metaMsg =
+        UploadImageRequest.newBuilder()
+          .run {
+            meta =
+              MetaData.newBuilder()
+                .run {
+                  auth = "secret"
+                  fileName = filename.split(".")[0]
+                  imageFormat = filename.split(".")[1]
+                  build()
+                }
+            build()
+          }
+      requestObserver.onNext(metaMsg)
+
+      // Grab the semaphone to start
+      semaphore.acquire()
+
+      FileInputStream(file)
+        .use { fis ->
+          val bis = BufferedInputStream(fis)
+          val buffer = ByteArray(bufferSize)
+          var byteCount: Int
+
+          while (bis.read(buffer).also { bytesRead -> byteCount = bytesRead } > 0) {
+            clientByteCount += byteCount
+            clientChunkCount++
+            clientChecksum.update(buffer, 0, buffer.size);
+            val byteString: ByteString = ByteString.copyFrom(buffer)
+
+            val req =
+              UploadImageRequest.newBuilder()
+                .let { builder ->
+                  builder.data =
+                    ChunkData.newBuilder()
+                      .run {
+                        chunkCount = clientChunkCount
+                        chunkByteCount = byteCount
+                        chunkChecksum = clientChecksum.value
+                        chunkBytes = byteString
+                        build()
+                      }
+                  builder.build()
                 }
 
-                override fun onError(t: Throwable) {
-                    val status = Status.fromThrowable(t)
-                    logger.info { "uploadImage() failed: $status" }
-                    finishLatch.countDown()
-                }
+            logger.info { "Writing $clientChunkCount of $bufferSize" }
+            requestObserver.onNext(req)
 
-                override fun onCompleted() {
-                    finishLatch.countDown()
-                }
+            logger.info { "Waiting for semaphore" }
+            try {
+              semaphore.tryAcquire(1, TimeUnit.SECONDS)
+            } catch (e: InterruptedException) {
+              e.printStackTrace()
+              // Deal with timeout situation
+              // For now, just return
+              return
             }
 
-        val requestObserver = asyncStub.uploadImage(responseObserver)
+            logger.info { "Acquired semaphore" }
 
-        try {
-            val file = File(filepath)
-            if (!file.exists()) {
-                logger.info { "File does not exist" }
-                return
+            if (finishLatch.count == 0L) {
+              // RPC completed or errored before we finished sending.
+              // Sending further requests won't error, but they will just be thrown away.
+              return
             }
+          }
+        }
 
-            val filename = filepath.split("/").lastOrNull()!!
-
-            val metaMsg =
-                UploadImageRequest.newBuilder()
-                    .run {
-                        meta =
-                            MetaData.newBuilder()
-                                .run {
-                                    auth = "secret"
-                                    fileName = filename.split(".")[0]
-                                    imageFormat = filename.split(".")[1]
-                                    build()
-                                }
-                        build()
-                    }
-            requestObserver.onNext(metaMsg)
-
-            // Grab the semaphone to start
-            semaphore.acquire()
-
-            FileInputStream(file)
-                .use { fis ->
-                    val bis = BufferedInputStream(fis)
-                    val buffer = ByteArray(bufferSize)
-                    var byteCount: Int
-
-                    while (bis.read(buffer).also { bytesRead -> byteCount = bytesRead } > 0) {
-                        clientByteCount += byteCount
-                        clientChunkCount++
-                        clientChecksum.update(buffer, 0, buffer.size);
-                        val byteString: ByteString? = ByteString.copyFrom(buffer)
-
-                        val req =
-                            UploadImageRequest.newBuilder()
-                                .let { builder ->
-                                    builder.data =
-                                        ChunkData.newBuilder()
-                                            .run {
-                                                chunkCount = clientChunkCount
-                                                chunkByteCount = byteCount
-                                                chunkChecksum = clientChecksum.value
-                                                chunkBytes = byteString
-                                                build()
-                                            }
-                                    builder.build()
-                                }
-
-                        logger.info { "Writing $clientChunkCount of $bufferSize" }
-                        requestObserver.onNext(req)
-
-                        logger.info { "Waiting for semaphore" }
-                        try {
-                            semaphore.tryAcquire(1, TimeUnit.SECONDS)
-                        } catch (e: InterruptedException) {
-                            e.printStackTrace()
-                            // Deal with timeout situation
-                            // For now, just return
-                            return
-                        }
-
-                        logger.info { "Acquired semaphore" }
-
-                        if (finishLatch.count == 0L) {
-                            // RPC completed or errored before we finished sending.
-                            // Sending further requests won't error, but they will just be thrown away.
-                            return
-                        }
-                    }
+      val summaryMsg =
+        UploadImageRequest.newBuilder()
+          .run {
+            summary =
+              SummaryData.newBuilder()
+                .run {
+                  summaryChunkCount = clientChunkCount
+                  summaryByteCount = clientByteCount
+                  summaryChecksum = clientChecksum.value
+                  build()
                 }
+            build()
+          }
+      requestObserver.onNext(summaryMsg)
 
-            val summaryMsg =
-                UploadImageRequest.newBuilder()
-                    .run {
-                        summary =
-                            SummaryData.newBuilder()
-                                .run {
-                                    summaryChunkCount = clientChunkCount
-                                    summaryByteCount = clientByteCount
-                                    summaryChecksum = clientChecksum.value
-                                    build()
-                                }
-                        build()
-                    }
-            requestObserver.onNext(summaryMsg)
+      // Mark the end of requests
+      requestObserver.onCompleted()
 
-            // Mark the end of requests
-            requestObserver.onCompleted()
-
-        } catch (e: RuntimeException) {
-            // Cancel RPC
-            requestObserver.onError(e)
-            throw e
-        }
-
-        logger.info { "Client streaming is complete" }
-
-        // Receiving happens asynchronously
-        try {
-            finishLatch.await(10, TimeUnit.MINUTES)
-        } catch (e: InterruptedException) {
-            e.printStackTrace()
-        }
-
-        logger.info { "Client exiting" }
-
+    } catch (e: RuntimeException) {
+      // Cancel RPC
+      requestObserver.onError(e)
+      throw e
     }
 
-    override fun close() {
-        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
+    logger.info { "Client streaming is complete" }
+
+    // Receiving happens asynchronously
+    try {
+      finishLatch.await(10, TimeUnit.MINUTES)
+    } catch (e: InterruptedException) {
+      e.printStackTrace()
     }
 
-    companion object : KLogging() {
-        // See: https://github.com/grpc/grpc.github.io/issues/371
-        const val bufferSize = 32 * 1024
+    logger.info { "Client exiting" }
 
-        @JvmStatic
-        fun main(args: Array<String>) {
-            ChunkerClient("localhost")
-                .use { client ->
-                    client.uploadImage("data/AutumScene.jpg")
-                }
+  }
+
+  override fun close() {
+    channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
+  }
+
+  companion object : KLogging() {
+    // See: https://github.com/grpc/grpc.github.io/issues/371
+    const val bufferSize = 32 * 1024
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+      ChunkerClient("localhost")
+        .use { client ->
+          client.uploadImage("data/AutumScene.jpg")
         }
     }
+  }
 }
--- src/main/kotlin/org/athenian/chunker/ChunkerServer.kt
+++ src/main/kotlin/org/athenian/chunker/ChunkerServer.kt
@@ -7,33 +7,33 @@ import java.io.IOException
 
 class ChunkerServer {
 
-    private var server: Server? = null
+  private var server: Server? = null
 
-    @Throws(IOException::class)
-    private fun start() {
-        server = ServerBuilder.forPort(port).addService(ChunkerImpl()).build().start()
-        logger.info { "Server started, listening on $port" }
-        Runtime.getRuntime().addShutdownHook(
-            Thread {
-                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-                System.err.println("*** shutting down gRPC server since JVM is shutting down")
-                server?.shutdown()
-                System.err.println("*** server shut down")
-            })
-    }
+  @Throws(IOException::class)
+  private fun start() {
+    server = ServerBuilder.forPort(port).addService(ChunkerImpl()).build().start()
+    logger.info { "Server started, listening on $port" }
+    Runtime.getRuntime().addShutdownHook(
+      Thread {
+        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+        System.err.println("*** shutting down gRPC server since JVM is shutting down")
+        server?.shutdown()
+        System.err.println("*** server shut down")
+      })
+  }
 
-    companion object : KLogging() {
+  companion object : KLogging() {
 
-        const val port = 50051
+    const val port = 50051
 
-        @Throws(IOException::class, InterruptedException::class)
-        @JvmStatic
-        fun main(args: Array<String>) {
-            ChunkerServer()
-                .apply {
-                    start()
-                    server?.awaitTermination()
-                }
+    @Throws(IOException::class, InterruptedException::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+      ChunkerServer()
+        .apply {
+          start()
+          server?.awaitTermination()
         }
     }
+  }
 }
